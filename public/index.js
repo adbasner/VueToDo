7ea4fe61c7ddf42290b53d4a29bd624ef80e3d77
Andrew Basner <andrewbasner@gmail.com>
2018-08-06 19:34:18 -0400

    make todo validation

--- public/index.js
+++ public/index.js
@@ -5,10 +5,58 @@ var HomePage = {
       message: "Welcome to Vue.js!",
       message2: "message2",
       message3: "sldfjasf",
+      tasks: 
+      [
+        {
+          id: 1,
+          todo: "Something",
+          complete: false
+        },
+        {
+          id: 2,
+          todo: "Something else",
+          complete: false 
+        },
+        {
+          id: 3,
+          todo: "Something more",
+          complete: true 
+        }
+      ],
+      newTodo:
+      {
+        id: "",
+        todo: "",
+        complete: false
+      }
     };
   },
   created: function() {},
-  methods: {},
+  methods: {
+    addTodo: function() {
+      console.log("Adding item to todo list");
+      var newId = this.tasks.length + 1;
+      var newTodoItem = {
+        id: newId,
+        todo: this.newTodo.todo,
+        complete: false
+      };
+
+      if (this.newTodo.todo === "") {
+        console.log('Empty todo, not added');
+        // this.tasks.push({         
+        //   id: newId,
+        //   todo: "EMPTY",
+        //   complete: false 
+        // });
+      } else {
+        this.tasks.push(newTodoItem);
+        console.log('Added a todo');
+      }
+
+      this.newTodo.todo = "";
+    }
+  },
   computed: {}
 };
 
